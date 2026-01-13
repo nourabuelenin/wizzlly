3a55fa0bd4f6e05c55d9aa1beed17d2152a3c1ae
MustafaAFarag <mustafa.ashraf.saad@gmail.com>
2026-01-13 14:07:50 +0200

    Button Reuseability

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export default function Button({ text, ShowArrow }: ButtonProps) {
   return (
-    <button className="inline-flex gap-5 bg-white text-black p-3 rounded-full text-xl items-center w-fit ">
+    <button className="inline-flex cursor-pointer gap-5 bg-white text-black p-3 rounded-full text-xl items-center w-fit ">
       {text}
       {ShowArrow && (
         <span className="bg-primary text-white rounded-full p-3">
