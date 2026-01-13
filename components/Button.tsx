d62266b9f21b195e48b0d9f45c544afb3bb3ec54
MustafaAFarag <mustafa.ashraf.saad@gmail.com>
2026-01-13 14:09:30 +0200

    Button Animated

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,10 +7,10 @@ interface ButtonProps {
 
 export default function Button({ text, ShowArrow }: ButtonProps) {
   return (
-    <button className="inline-flex cursor-pointer gap-5 bg-white text-black p-3 rounded-full text-xl items-center w-fit ">
+    <button className="inline-flex cursor-pointer gap-5 bg-white text-black p-3 rounded-full text-xl items-center w-fit transition-all duration-300 hover:scale-105 hover:shadow-lg">
       {text}
       {ShowArrow && (
-        <span className="bg-primary text-white rounded-full p-3">
+        <span className="bg-primary text-white rounded-full p-3 transition-transform duration-300 group-hover:translate-x-1">
           <ArrowRight className="rtl:rotate-180" />
         </span>
       )}
