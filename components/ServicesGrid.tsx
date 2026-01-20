60877aff353e8e3bb73ae91a0139b4d3861db869
MustafaAFarag <mustafa.ashraf.saad@gmail.com>
2026-01-20 20:22:58 +0200

    What we do seciton for all devices

--- components/ServicesGrid.tsx
+++ components/ServicesGrid.tsx
@@ -6,7 +6,7 @@ export const ServicesGrid = ({
 }: {
   services: ServiceCardProps[];
 }) => (
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-10">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-4 md:gap-5 lg:gap-6 xl:gap-10">
     {services.map((service, index) => (
       <ServiceCard key={index} {...service} />
     ))}
