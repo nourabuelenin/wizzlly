f43a431bdf1a43424e42464f70e444f3ffe58943
MustafaAFarag <mustafa.ashraf.saad@gmail.com>
2026-01-20 20:28:27 +0200

    How we work

--- components/HomepageHowWeWork.tsx
+++ components/HomepageHowWeWork.tsx
@@ -4,24 +4,24 @@ import { Zap } from "lucide-react";
 export default function HomepageHowWeWork({ dict }: any) {
   return (
     <section className="bg-linear-to-tr from-[#032d79] to-[#0eaeef] -mt-5 py-10 md:py-24 rounded-t-2xl text-white">
-      <div className="container mx-auto px-4 md:px-10 grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-8 md:gap-16 items-start">
+      <div className="container mx-auto px-4 md:px-10 grid grid-cols-1 xl:grid-cols-[1fr_2fr] gap-8 md:gap-8 lg:gap-10 xl:gap-16 items-start">
         {/* Left Side */}
-        <div className="flex flex-col gap-4 md:gap-6 h-full justify-center">
+        <div className="flex flex-col gap-4 md:gap-6 h-full justify-center items-center xl:items-start text-center xl:text-start">
           <span className="border border-white/70 w-fit rounded-full px-4 md:px-5 py-1.5 md:py-2 text-sm md:text-lg tracking-wide">
             {dict.homepage.howWeWork.badge}
           </span>
 
-          <h3 className="font-medium text-3xl md:text-4xl lg:text-6xl leading-tight">
+          <h3 className="font-medium text-4xl md:text-5xl lg:text-6xl xl:text-6xl leading-tight">
             {dict.homepage.howWeWork.headline}
           </h3>
 
-          <p className="text-lg md:text-xl lg:text-2xl text-white/90 max-w-xl">
+          <p className="text-lg md:text-2xl lg:text-2xl text-white/90 max-w-xl">
             {dict.homepage.howWeWork.description}
           </p>
         </div>
 
         {/* Right Side */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-5 lg:gap-6 xl:gap-6">
           {dict.homepage.howWeWork.steps.map((title: string, index: number) => (
             <div
               key={title}
