e1e480609e3131833eadb9a8127e4a9f3c6318e8
nourabuelenin <nourabuelenin@gmail.com>
2026-02-20 15:38:15 +0200

    chore: cloudflare build fixes

---
+++ open-next.config.ts
@@ -0,0 +1,24 @@
+import cache from "@opennextjs/cloudflare/kvCache";
+
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: async () => cache,
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+};
+
+export default config;
